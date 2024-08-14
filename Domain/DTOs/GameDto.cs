74ace4c8409b50649c3ffbdc32a23a07360296db
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-08-14 11:21:51 -0300

    :sparkles: feat: User and Game DTOs

---
+++ Domain/DTOs/GameDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.DTOs
+{
+    public class GameDto
+    {
+        public string Name { get; set; }
+        public string plataform { get; set; }
+        public int Ano {  get; set; }
+    }
+}
---
+++ Domain/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+
+namespace Domain.DTOs
+{
+    public class UserDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
