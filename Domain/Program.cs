6365a542fa8e00f0b49a1be57408f2ac28a05dcb
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-08-14 16:34:18 -0300

    :sparkles: feat: Services and DataContext Configuration

--- Domain/Program.cs
+++ Domain/Program.cs
@@ -1,5 +1,33 @@
+using Domain.Infra.Contexts;
+using Domain.Services.GameServices;
+using Domain.Services.UserServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddScoped<IGameService, GameService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseEndpoints(endpoints =>
+{
+
+});
 
 app.MapGet("/", () => "Hello World!");
 
