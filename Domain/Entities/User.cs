6eee62609934163ac098d0feb4a88de407ed7dd8
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-08-14 16:40:20 -0300

    :recycle: refactor: Alter Id property name

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -6,13 +6,13 @@ namespace Domain.Entities
     {
         public User(string email, string password, Role role)
         {
-            Guid = Guid.NewGuid();
+            Id = Guid.NewGuid();
             Email = email;
             Password = password;
             Role = role;
         }
 
-        public Guid Guid { get; private set; }
+        public Guid Id { get; private set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public Role Role { get; set; }
