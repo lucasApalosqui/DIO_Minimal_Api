4ebf2bfa494bc629fe81ef7971cf97063a816c4f
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-08-14 16:36:21 -0300

    :sparkles: feat: First Migration

---
+++ Domain/Migrations/20240814192954_FirstMigration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tbl_Game",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "VARCHAR(256)", maxLength: 256, nullable: false),
+                    Plataform = table.Column<string>(type: "VARCHAR(256)", maxLength: 256, nullable: false),
+                    Ano = table.Column<short>(type: "SMALLINT", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tbl_Game", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR(256)", maxLength: 256, nullable: false),
+                    Password = table.Column<string>(type: "VARCHAR(256)", maxLength: 256, nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tbl_Game");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
